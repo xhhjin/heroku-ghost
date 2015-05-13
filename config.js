ef7c8e3da9fc5b3cc9d78a8839324212e6db1346
xhhjin <xhhjin@gmail.com>
2015-05-13 15:10:05 +0800

    update

--- config.js
+++ config.js
@@ -47,11 +47,11 @@ config = {
         database: {
         	client: 'postgres',
             connection: {
-				host: process.env.POSTGRES_HOST,
-				user: process.env.POSTGRES_USER,
-				password: process.env.POSTGRES_PASSWORD,
-				database: process.env.POSTGRES_DATABASE, 
-				port: '5432'
+                host: process.env.POSTGRES_HOST,
+                user: process.env.POSTGRES_USER,
+                password: process.env.POSTGRES_PASSWORD,
+                database: process.env.POSTGRES_DATABASE, 
+                port: '5432'
             },
             debug: false
         },
