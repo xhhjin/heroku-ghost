9cfe479cc99e8bebcb3b7041e0d652efb118a215
xhhjin <xhhjin@gmail.com>
2015-05-14 16:52:44 +0800

    关闭新版本提示。

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         url: 'http://ghost.xuhehuan.com',
+		UpdateCheck: false;
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
